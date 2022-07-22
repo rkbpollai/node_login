dfbd95b7a0f0fb97cd563a3d7e5a9143d1aab9ac
rkbpollai <109358435+rkbpollai@users.noreply.github.com>
2022-07-22 13:53:07 +0530

    this is commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT||3000;
+app.set('view engine','ejs');
+app.get('/',(req,res)=>(
+   res.render('base',{title:'Login System'})
+))
+app.listen(port,()=>(console.log("lostening to the server on http://localhost:3000")));
